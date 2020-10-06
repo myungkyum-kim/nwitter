a950f6483777a40fff725acaad90d306cd5dade1
Myungkyum Kim <myungyum.kim@sunnyfive.net>
2020-10-06 19:51:18 +0900

    Updates

---
+++ precache-manifest.06835f934f26f8856ef03462248d2c46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180d13f837f975e89aed7fd4a696ef82",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "39a771e2d083ba7cdf9c",
+    "url": "/nwitter/static/js/2.015c208e.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/nwitter/static/js/2.015c208e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201d50e8c6dad6d8a9f0",
+    "url": "/nwitter/static/js/main.dd9d82ea.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
